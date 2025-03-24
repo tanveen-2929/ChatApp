f69b50617d15e44f7afa31a25407f593f64ee963
tanveen-2929 <tanveenk66@gmail.com>
2025-03-24 12:43:11 +0530

    message work

--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,36 @@
 import express from "express";
 import dotenv from "dotenv";
-import mongoose from 'mongoose';
-import userRoute from './route/user.route.js';
-import cors from 'cors';
+import mongoose from "mongoose";
+import userRoute from "./route/user.route.js";
+import cors from "cors";
 import cookieParser from "cookie-parser";
+import messageRoute from "./route/message.route.js";
 
 const app = express();
 dotenv.config();
+
 app.use(express.json());
 
 app.use(cors());
 app.use(cookieParser());
 
 const PORT = process.env.PORT || 5002;
-const URI = process.env.MONGODB_URI; 
-
-console.log("MongoDB URI:", URI); // Log the MongoDB URI for debugging
-
-try {
-    await mongoose.connect(URI);
-    console.log("MongoDB Connected");
-    app.listen(PORT, () => {
-        console.log(`Server is Running on port ${PORT}`);
-    });
-} catch (error) {
-    console.log(error);
-}
+const URI = process.env.MONGODB_URI;
 
 app.use("/api/user", userRoute);
+app.use("/api/message", messageRoute);
+
+// Handle 404 errors for undefined routes
+app.use((req, res) => {
+  res.status(404).json({ message: "Requested resource could not be found." });
+});
+
+try {
+  await mongoose.connect(URI);
+  console.log("MongoDB Connected");
+  app.listen(PORT, () => {
+    console.log(`Server is Running on port ${PORT}`);
+  });
+} catch (error) {
+  console.log(error);
+}
---
+++ backend/route/message.route.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { sendMessage } from "../controller/message.controller.js";
+import secureRoute from "../middleware/secureRoute.js";
+
+const router = express.Router();
+router.post("/send/:id", secureRoute, sendMessage);
+
+export default router;
