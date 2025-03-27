4e2024b427d3dac047f0d33c36c1de05a03b2dc4
tanveen-2929 <tanveenk66@gmail.com>
2025-03-27 09:38:45 +0530

    final app complete

--- backend/frontend/src/context/SocketContext.jsx
+++ backend/frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:5002", {
+      const socket = io("https://chatapp-0xsv.onrender.com", {
         query: {
           userId: authUser.user._id,
         },
